2ea8e2a6be0f79242bfcf98db8f153f93a286237
Marichka <marichka.vyalik@gmail.com>
2021-12-05 14:18:01 +0200

    changes

--- wp-content/themes/simple/footer.php
+++ wp-content/themes/simple/footer.php
@@ -1,13 +1,13 @@
-<div class="clear"></div>
   <!-- Подвал сайта -->
-    <footer id="colophon" class="site-footer">
+  <footer id="colophon" class="site-footer">
       <div class="site-info">
-        <a href="<?php echo esc_url( __( 'http://wordpress.org/' ) ); ?>">
-          <?php echo 'KIT 2020'; ?>
-        </a>
+          <a href="<?php echo esc_url(__('http://wordpress.org/')); ?>">
+              <?php echo 'KIT 2020'; ?>
+          </a>
       </div><!-- .site-info -->
-      <?php wp_footer(); ?>
-    </footer><!-- #colophon -->
+      <?php wp_footer();?>
+  </footer><!-- #colophon -->
   </div><!-- #page -->
-</body>
-</html>
+  </body>
+
+  </html>
