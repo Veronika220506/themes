bc09b5490e30044af59b78572889522ae662a53f
Marichka <marichka.vyalik@gmail.com>
2021-12-05 13:16:40 +0200

    add new files php

---
+++ wp-content/themes/simple/footer.php
@@ -0,0 +1,13 @@
+<div class="clear"></div>
+  <!-- Подвал сайта -->
+    <footer id="colophon" class="site-footer">
+      <div class="site-info">
+        <a href="<?php echo esc_url( __( 'http://wordpress.org/' ) ); ?>">
+          <?php echo 'KIT 2020'; ?>
+        </a>
+      </div><!-- .site-info -->
+      <?php wp_footer(); ?>
+    </footer><!-- #colophon -->
+  </div><!-- #page -->
+</body>
+</html>
